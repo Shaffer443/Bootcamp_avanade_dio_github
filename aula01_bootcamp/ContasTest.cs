efb794be28a1724f8a89dd33708de21e3451bef8
Shaffer443 <rafaelgouveiamelo@yahoo.com.br>
2021-12-15 11:50:24 -0300

    subindo todos os arquivos trabalhados no projeto Visual Studio c# do bootcamp Dio

---
+++ aula01_bootcamp/ContasTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aula01_bootcamp
+{
+    internal class ContasTest
+    {
+        public class Contas
+        {
+
+            public int Soma(int a, int b)
+            {
+                return a + b;
+            }
+            public int Subtracao(int a, int b)
+            {
+                return a - b;
+            }
+            public int Multiplicacao(int a, int b)
+            {
+                return a * b;
+            }
+            public int Divisao(int a, int b)
+            {
+                return a / b;
+            }
+
+
+        }
+
+    }
+}
--- aula01_bootcamp/Program.cs
+++ aula01_bootcamp/Program.cs
@@ -13,5 +13,57 @@ namespace aula01_bootcamp
             Console.WriteLine(a+b); //Concatenação
             Console.WriteLine(a+" "+b);//Concatenação 2. Assim ele concatena com espaço, mas também pode-se da espaço direto na string da variável.
         }
+    
+       // public void SomaTest()
+        {
+
+            //arrange
+           // var x = 5;
+           // var y = 5;
+           // var esperado = 10;
+           // var teste = new Teste();
+
+            //act
+
+            //var resultado;
+
+            //assert
+
+            //Assert.AreEqual(esperado, result);
+
+
+        }
+
+        public class Contas
+        {
+
+            public int Soma(int a, int b)
+            {
+                return a + b;
+            }
+            public int Subtracao(int a, int b)
+            {
+                return a - b;
+            }
+            public int Multiplicacao(int a, int b)
+            {
+                return a * b;
+            }
+            public int Divisao(int a, int b)
+            {
+                return a / b;
+            }
+
+
+        }
+    
+    
+    
     }
+    
+
+
 }
+
+
+
