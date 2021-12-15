4f7d23d1e956f93075a612e8d8c130b3fd30f05a
Shaffer443 <rafaelgouveiamelo@yahoo.com.br>
2021-12-15 10:16:49 -0300

    arquivos das aulas de c#

---
+++ aula01_bootcamp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace aula01_bootcamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Rafael"; 
+            var b = "Gouveia";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(a,b); //Imprimir apenas a. 
+            Console.WriteLine(a+b); //Concatenação
+            Console.WriteLine(a+" "+b);//Concatenação 2. Assim ele concatena com espaço, mas também pode-se da espaço direto na string da variável.
+        }
+    }
+}
